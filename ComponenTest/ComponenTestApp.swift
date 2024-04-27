661d55aa247ab4a697b98b69c39d74c5fde2664c
heezoochoi <heezoo.choi@gmail.com>
2024-04-27 17:02:42 +0900

    프로젝트 세팅

---
+++ ComponenTest/ComponenTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComponenTestApp.swift
+//  ComponenTest
+//
+//  Created by 최희주 on 4/27/24.
+//
+
+import SwiftUI
+
+@main
+struct ComponenTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComponenTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ComponenTest
+//
+//  Created by 최희주 on 4/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
